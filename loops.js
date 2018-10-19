0e09d151c3cff9b8c5845fa325a532455ba7f5ce
Ian Zaragoza <ianzaragoza@msn.com>
2018-10-19 16:34:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,12 +17,9 @@ function whileLoop(n) {
   return "done";
 }
 
-function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-   do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
+function doWhileLoop(array){
+do {
+  array.pop()
+} while (array.length>0 && maybeTrue())
+return array
 }
